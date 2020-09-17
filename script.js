21593765aef94c57bf322f48385e57bead0135b6
Andrew Crow <andrew@crow.me>
2020-09-17 01:13:59 -0700

    moved time-block to the row

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ function renderTimeBlocks() {
     for (let i = 1; i < 9; i++) {
         let button = "button";
         let textArea = "textArea";
-        let newRow = $("<form>").addClass("row");
-        let newTimeBox = $("<label>").addClass("col-2 hour time-block").attr("for", textArea + i);
+        let newRow = $("<form>").addClass("row time-block");
+        let newTimeBox = $("<label>").addClass("col-2 hour").attr("for", textArea + i);
         let newDescription = $("<textarea>").addClass("col-9 description past").attr("id", textArea + i).attr("type", "text");
         let newButton = $("<button>").addClass("col saveBtn d-flex justify-content-center align-items-center").attr("id", button + i).attr("type", "submit");
         let newSaveIcon = $("<i>").addClass("fas fa-save");
