9d247111578f51ab04ac313159455f0b70feda8d
Andrew Crow <andrew@crow.me>
2020-09-17 01:09:44 -0700

    added event listener to buttons that outputs the button id, and send it ot new function to store the id

--- script.js
+++ script.js
@@ -2,16 +2,35 @@
 
 
 function renderTimeBlocks() {
-    for (let i = 0; i < 9; i++) {
+    for (let i = 1; i < 9; i++) {
+        let button = "button";
+        let textArea = "textArea";
         let newRow = $("<form>").addClass("row");
-        let newTimeBox = $("<label>").addClass("col-2 hour time-block").attr("for", i);
-        let newDescription = $("<textarea>").addClass("col-9 description past").attr("id", i);
-        let newButton = $("<button>").addClass("col saveBtn d-flex justify-content-center align-items-center");
+        let newTimeBox = $("<label>").addClass("col-2 hour time-block").attr("for", textArea + i);
+        let newDescription = $("<textarea>").addClass("col-9 description past").attr("id", textArea + i).attr("type", "text");
+        let newButton = $("<button>").addClass("col saveBtn d-flex justify-content-center align-items-center").attr("id", button + i).attr("type", "submit");
         let newSaveIcon = $("<i>").addClass("fas fa-save");
         $(newRow).append(newTimeBox).append(newDescription).append(newButton);
         $(newButton).append(newSaveIcon);
         $(".container").append(newRow);
     };
+    $(".saveBtn").click(function (event) {
+        event.preventDefault();
+        let buttonId;
+        if (event.target.id === "") {
+            buttonId = event.target.parentElement.id;
+        } else {
+            buttonId = event.target.id;
+        }
+        storeTask(buttonId)
+    })
 }
+
+function storeTask(buttonId) {
+
+    console.log(buttonId);
+}
+
+
 
 renderTimeBlocks();
